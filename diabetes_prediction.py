91b214305c8eb571412044b36c29f39732c0392c
fariba-data <f.fallah0035@gmail.com>
2026-06-13 20:16:51 +0330

    Update diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -7,4 +7,15 @@ url = "https://raw.githubusercontent.com/plotly/datasets/master/diabetes.csv"
 
 data = pd.read_csv(url)
 
+# Display the first five rows
 print(data.head())
+
+# Features and target
+X = data.drop("Outcome", axis=1)
+y = data["Outcome"]
+
+print("Features:")
+print(X.head())
+
+print("Target:")
+print(y.head())
